0925d822f4742aff8afff63a03a5a5b8939af641
Ravi Bhanawat <ravibhanawat@Ravis-MacBook-Air.local>
2023-04-28 01:46:11 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf')
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'main',
+        remotes: {
+          widget: `widget@http://localhost:3002/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+        },
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './footer': './src/components/footers/index.tsx',
+
+        },
+        shared: {
+          // whatever else
+        },
+      })
+    );
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
